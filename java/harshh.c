5b44a76fc98a18fa6d108b72b36c55b10ed5cb6c
harsh-rajpal <harshrajpal2k18@gmail.com>
2021-03-17 10:46:53 +0530

    done

---
+++ java/harshh.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    char st;
+    
+    printf("enter the value\n");
+    scanf("%c",&st);
+    
+    printf("Hello, World!\n");
+    printf("%s\n",st);
+    return 0;
+}
